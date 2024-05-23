c4ff07c7aa38b6e67fe903d18b8cfaadcdb34f78
eddayyy <eduardonunez@csu.fullerton.edu>
2024-05-23 11:09:57 -0700

    Updated tracked files

--- FEOptionBrew/optionbrew/src/pages/Dashboard/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Dashboard.css";
-
-const Dashboard = () => {
-  return <h1>This is the Dashboard</h1>;
-};
-
-export default Dashboard;
--- FEOptionBrew/optionbrew/src/pages/Market/Market.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Market.css";
-
-const Market = () => {
-  return <h1>This is the Market</h1>;
-};
-
-export default Market;
